f8e2e8aa034416c7458d1b2f8f700f49f7e08c16
Haxrox <56497846+Haxrox@users.noreply.github.com>
2021-05-30 16:51:36 -0700

    implemented option to re-pick a show

--- EpisodeGenerator.py
+++ EpisodeGenerator.py
@@ -3,16 +3,19 @@ import ShowsData
 
 Shows = ShowsData.Data
 
+# Preparing query to send to the user
+query = "What show do you want to watch? (enter number)\n"
+for index in range(len(Shows)):
+    query = query + "{}. {}\n".format(index + 1, Shows[str(index + 1)]['Name'])
+query = query + "{}. Any\n".format(len(Shows) + 1)
+
+# Get the episode based off the show
 def getEpisode(episodes):
     season = random.randint(0, len(episodes['Seasons']) - 1)
     episode = random.randint(1, episodes['Seasons'][season])            
     print("Watching {} season {} episode {}".format(episodes['Name'], season + 1, episode))
 
-if __name__ == '__main__':
-    query = "What show do you want to watch? (enter number)\n"
-    for index in range(len(Shows)):
-        query = query + "{}. {}\n".format(index + 1, Shows[str(index + 1)]['Name'])
-    query = query + "{}. Any\n".format(len(Shows) + 1)
+def getShow():
     num = input(query)
     
     if len(num) == 0 or num == str(len(Shows) + 1):
@@ -23,6 +26,9 @@ if __name__ == '__main__':
         except ValueError:
             num = str(random.randint(1, len(Shows)))
 
-    show = Shows[num]
-    showName = show['Name']        
-    getEpisode(show)
+    return Shows[num]
+
+if __name__ == '__main__':
+    getEpisode(getShow())
+    while input("Input R to pick another show or enter to exit ...\n").upper() == "R":
+        getEpisode(getShow())
