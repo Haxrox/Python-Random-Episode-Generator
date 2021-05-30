aa5e48ea0cd4bec2ef16c97643df0a44ed8d32a0
Haxrox <Haxrox@github.com>
2021-05-29 21:20:45 -0700

    commit source

---
+++ EpisodeGenerator.py
@@ -0,0 +1,29 @@
+import random
+import ShowsData
+import webbrowser
+
+Shows = ShowsData.Data
+
+def getEpisode(episodes):
+    season = random.randint(0, len(episodes['Seasons']) - 1)
+    episode = random.randint(1, episodes['Seasons'][season])            
+    print("Watching {} season {} episode {}".format(episodes['Name'], season + 1, episode))
+
+if __name__ == '__main__':
+    query = "What show do you want to watch? (enter number)\n"
+    for index in range(len(Shows)):
+        query = query + "{}. {}\n".format(index + 1, Shows[str(index + 1)]['Name'])
+    query = query + "{}. Any\n".format(len(Shows) + 1)
+    num = input(query)
+    
+    if len(num) == 0 or num == str(len(Shows) + 1):
+        num = str(random.randint(1, len(Shows)))
+    else: 
+        try: 
+            int(num)
+        except ValueError:
+            num = str(random.randint(1, len(Shows)))
+
+    show = Shows[num]
+    showName = show['Name']        
+    getEpisode(show)
---
+++ ShowsData.py
@@ -0,0 +1,14 @@
+Data = {
+    '1': {
+        'Name': "Friends",
+        'Seasons': [
+            24, 24, 25, 24, 24, 25, 24, 24, 24, 18 # number of episodes in each season
+        ]
+    },
+    '2': {
+        'Name': "Brooklyn 99",
+        'Seasons': [
+            22, 23, 23, 22, 22, 18, 13, 10
+        ]
+    }
+}
